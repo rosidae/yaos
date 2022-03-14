e3d29cbefdcb7cdca44a513c3e7a118608d51a81
rosidae <windows10vmstuff@gmail.com>
2022-03-13 21:12:50 -0500

    idk what to do from here

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
 all:
 	nasm -f bin source/main.asm -o yaos.bin
+
+run:
+	qemu-system-x86_64.exe .\yaos.bin
+
+mr: 
+	nasm -f bin source/main.asm -o yaos.bin
+	qemu-system-x86_64.exe .\yaos.bin
--- source/main.asm
+++ source/main.asm
@@ -1,13 +1,30 @@
+; authored by Rosidae
+; main.asm
+; main file for YAOS
+
 [org 0x7c00]
-char:
-    db 0
-mov ah, 0
-int 0x16
-mov al, [char]
-int 0x10
+call cls
+mov ah, 0x0e
+mov bx, var
 
-
-jmp $
+print:
+    mov al, [bx]
+    cmp al, 0
+    je end
+    int 0x10
+    inc bx
+    jmp print
+end:
+    jmp $
+var:
+    db "Welcome to YAOS, this is the bios", 0
+cls:
+    pusha
+    mov ah, 0x00
+    mov al, 0x03  ; text mode 80x25 16 colours
+    int 0x10
+    popa
+    ret
 
 
 times 510-($-$$) db 0
