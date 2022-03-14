7ce1a32b869de3cf9701de5e33fee8d24d3b9414
rosidae <windows10vmstuff@gmail.com>
2022-03-14 04:32:05 -0500

    im calling it here, im stuck

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
 all:
-	nasm -f bin source/boot/bootloader.asm -o yaos.img
+	nasm -f bin source/boot/bootloader.asm -o builds/yaos.img
+	nasm -f elf source/kernel/kernel_ent.asm -o builds/bins/kernel_ent.o
+	gcc -ffreestanding -m32 -g -c -o builds/bins/kernel.o source/kernel/kernel.c
+	nasm -f bin source/kernel/zeros.asm -o builds/bins/zeros.bin
+	ld -o builds/bins/fkern.bin -Ttext 0x1000 builds/bins/kernel.o builds/bins/kernel_ent.o
+	type builds/yaos.img builds/bins/fkern.bin builds/bins/zeros.bin > builds/final-yaos.img
 run:
-	qemu-system-x86_64.exe .\yaos.img
+	qemu-system-x86_64.exe builds/final-yaos.img
 
 mr: 
-	nasm -f bin source/boot/bootloader.asm -o yaos.bin
-	qemu-system-x86_64.exe .\yaos.bin
+	nasm -f bin source/boot/bootloader.asm -o builds/yaos.img
+	nasm -f elf source/kernel/kernel_ent.asm -o builds/bins/kernel_ent.o
+	gcc -ffreestanding -lgcc -m32 -g -c -o builds/bins/kernel.o source/kernel/kernel.c
+	nasm -f bin source/kernel/zeros.asm -o builds/bins/zeros.bin
+	ld -o builds/bins/fkern.bin -Ttext 0x1000 builds/bins/kernel.o builds/bins/kernel_ent.o
+	type builds/yaos.img builds/bins/fkern.bin builds/bins/zeros.bin > builds/final-yaos.img
+	qemu-system-x86_64.exe builds/final-yaos.img
--- source/boot/bootloader.asm
+++ source/boot/bootloader.asm
@@ -1,25 +1,90 @@
-; authored by Rosidae
-; boot/bootloader.asm
-; bootloader for YAOS
+[org 0x7c00]                        
+KERNEL_LOCATION equ 0x1000
+                                    
 
-[org 0x7c00]
-mov ah, 0x0e
-mov bx, var
+mov [BOOT_DISK], dl                 
 
-pntstr:
-    mov al, [bx]
-    cmp al, 0
-    je end
-    int 0x10
-    inc bx
-    jmp pntstr
+                                    
+xor ax, ax                          
+mov es, ax
+mov ds, ax
+mov bp, 0x8000
+mov sp, bp
+
+mov bx, KERNEL_LOCATION
+mov dh, 2
+
+mov ah, 0x02
+mov al, dh 
+mov ch, 0x00
+mov dh, 0x00
+mov cl, 0x02
+mov dl, [BOOT_DISK]
+int 0x13
+
+                                    
+mov ah, 0x0
+mov al, 0x3
+int 0x10
 
 
-end:
-    jmp $
+CODE_SEG equ GDT_code - GDT_start
+DATA_SEG equ GDT_data - GDT_start
 
-var:
-    db "this is NOT yaos", 0
+cli
+lgdt [GDT_descriptor]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+jmp CODE_SEG:start_protected_mode
 
-times 510-($-$$) db 0
-db 0x55, 0xaa
+jmp $
+                                    
+BOOT_DISK: db 0
+
+GDT_start:
+    GDT_null:
+        dd 0x0
+        dd 0x0
+
+    GDT_code:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10011010
+        db 0b11001111
+        db 0x0
+
+    GDT_data:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10010010
+        db 0b11001111
+        db 0x0
+
+GDT_end:
+
+GDT_descriptor:
+    dw GDT_end - GDT_start - 1
+    dd GDT_start
+
+
+[bits 32]
+start_protected_mode:
+    mov ax, DATA_SEG
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	
+	mov ebp, 0x90000
+	mov esp, ebp
+
+    jmp KERNEL_LOCATION
+
+                                     
+ 
+times 510-($-$$) db 0              
+dw 0xaa55
