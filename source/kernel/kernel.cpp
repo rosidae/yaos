a8920aa82c31df75687e1d9a05f4ea20ba9c5085
rosidae <windows10vmstuff@gmail.com>
2022-03-15 14:42:11 -0500

    bro

--- source/kernel/kernel.cpp
+++ source/kernel/kernel.cpp
@@ -1,4 +1,17 @@
-void main(){
-    
+extern "C" void kputs(char* str) {
+	char* vidmem = (char*)0xb8000;
+	while (*str != '\0') {
+		*vidmem = *str;
+		vidmem+=2;
+		str++;
+	}
+}
+
+extern "C" void main(){
+	// char* vidmem = (char*)0xb8000;
+	// *vidmem = 'a';
+	// vidmem+=2;
+	// *vidmem = 'b';
+	kputs("h");
     return;
 }
