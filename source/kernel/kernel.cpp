e4b74f74c29cc3dd154f3486270b47d3935795e0
rosidae <windows10vmstuff@gmail.com>
2022-04-01 23:31:16 -0500

    👀
    👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀

--- source/kernel/kernel.cpp
+++ source/kernel/kernel.cpp
@@ -1,17 +1,20 @@
-extern "C" void kputs(char* str) {
-	char* vidmem = (char*)0xb8000;
-	while (*str != '\0') {
-		*vidmem = *str;
-		vidmem+=2;
-		str++;
-	}
+extern "C" void kputc(int pos, const char c) {
+    char* vidmem = (char*)0xB8000;
+    vidmem[2+(pos*2)+1] = c;
 }
 
-extern "C" void main(){
-	// char* vidmem = (char*)0xb8000;
-	// *vidmem = 'a';
-	// vidmem+=2;
-	// *vidmem = 'b';
-	kputs("h");
-    return;
+extern "C" int main(){
+    kputc(0, 't');
+    kputc(1, 'h');
+    kputc(2, 'i');
+    kputc(3, 's');
+    kputc(4, ' ');
+    kputc(5, 'i');
+    kputc(6, 's');
+    kputc(7, ' ');
+    kputc(8, 'y');
+    kputc(9, 'a');
+    kputc(10, 'o');
+    kputc(11, 's');
+    return 1;
 }
--- source/kernel/kernel_ent.asm
+++ source/kernel/kernel_ent.asm
@@ -1,5 +1,6 @@
 section .text
     [bits 32]
+    [extern puts]
     [extern main]
     call main
     jmp $
